2d76f4dbf66b92b15a5d71370ae172913c32bd29
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-24 16:33:13 +0300

    test functionality for different type of answers - implement later

--- skincare-quiz/src/pages/questions/QuestionOne.jsx
+++ skincare-quiz/src/pages/questions/QuestionOne.jsx
@@ -1,18 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import { removeItem, setItem } from '../../functions/updateLocalStorage';
+import { useContext, useState } from 'react';
+import ProductContext from '../../context/ProductContext';
 
 function QuestionOne() {
   const navigate = useNavigate();
+  const [answer, setAnswer] = useState('');
+  const { allProducts, product, setProduct } = useContext(ProductContext);
+
+  const filterProducts = (e) => {
+    const answer = e.target.textContent.toLowerCase();
+    console.log(answer);
+
+    const option = allProducts
+      .get('Hair Styling')
+      .filter((product) => product.tags.some((tag) => tag.includes(answer)));
+
+    console.log(option);
+  };
 
   return (
     <div className='question-block'>
       <div className='question'>
         <h1>What's your hair type or texture?</h1>
         <ul className='answers'>
-          <li>Straight</li>
-          <li>Curly</li>
-          <li>Wavy</li>
-          <li>Fine</li>
+          <li onClick={(e) => filterProducts(e)}>Straight</li>
+          <li onClick={(e) => filterProducts(e)}>Curly</li>
+          <li onClick={(e) => filterProducts(e)}>Wavy</li>
+          <li onClick={(e) => filterProducts(e)}>Fine</li>
         </ul>
         <div className='buttons'>
           <button
--- skincare-quiz/src/pages/questions/QuestionThree.jsx
+++ skincare-quiz/src/pages/questions/QuestionThree.jsx
@@ -1,21 +1,82 @@
 import { useNavigate } from 'react-router-dom';
 import { removeItem, setItem } from '../../functions/updateLocalStorage';
+import { useContext, useState } from 'react';
+import ProductContext from '../../context/ProductContext';
 
 function QuestionThree() {
   const navigate = useNavigate();
   const progressItem = JSON.parse(localStorage.getItem('progress'));
+  const { allProducts, product, setProduct } = useContext(ProductContext);
+
+  const filterProducts = (e) => {
+    const answer = e.target.textContent.toLowerCase();
+
+    if (
+      answer === 'anti-breakage' ||
+      answer === 'repairs appearance of damaged hair'
+    ) {
+      const optionOne = allProducts
+        .get('Hair Treatment')
+        .filter(
+          (product) =>
+            product.handle.includes('breakage') ||
+            product.handle.includes('hydrate')
+        );
+      const optionTwo = allProducts
+        .get('Hair Styling')
+        .filter(
+          (product) =>
+            product.handle.includes('heat-protection') ||
+            product.handle.includes('hydrate')
+        );
+      console.log(option, answer);
+    } else if (answer === 'hydration') {
+      const option = allProducts
+        .get('Duo Packs')
+        .filter((product) => product.handle.includes('hydrating'));
+
+      console.log(option, answer);
+    } else if (answer === 'soothing dry scalp') {
+      const option = allProducts
+        .get('Shampoo')
+        .filter(
+          (product) =>
+            product.handle.includes('scalp') ||
+            product.handle.includes('hydrating')
+        );
+
+      console.log(option, answer);
+    } else if (answer === 'volume') {
+      const optionOne = allProducts
+        .get('Duo Packs')
+        .filter((product) => product.handle.includes('volume'));
+
+      const optionTwo = allProducts
+        .get('Hair Styling')
+        .filter((product) => product.handle.includes('volumising'));
+
+      console.log(optionOne, optionTwo);
+    } else {
+      const option = allProducts
+        .get('Hair Styling')
+        .filter((product) => product.handle.includes('curl'));
+      console.log(option, answer);
+    }
+  };
 
   return (
     <div className='question-block'>
       <div className='question'>
         <h1>What benefit do you look for in your hair products?</h1>
         <ul className='answers'>
-          <li>Anti-breakage</li>
-          <li>Hydration</li>
-          <li>Soothing dry scalp</li>
-          <li>Repairs appearance of damaged hair</li>
-          <li>Volume</li>
-          <li>Curl and coil enhancing</li>
+          <li onClick={(e) => filterProducts(e)}>Anti-breakage</li>
+          <li onClick={(e) => filterProducts(e)}>Hydration</li>
+          <li onClick={(e) => filterProducts(e)}>Soothing dry scalp</li>
+          <li onClick={(e) => filterProducts(e)}>
+            Repairs appearance of damaged hair
+          </li>
+          <li onClick={(e) => filterProducts(e)}>Volume</li>
+          <li onClick={(e) => filterProducts(e)}>Curl and coil enhancing</li>
         </ul>
         <div className='buttons'>
           <button
