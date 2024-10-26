1fb0a17a67f44b726270b60e201c4545823fdb4e
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-26 14:27:10 +0300

    rename class and write add the active class again - prev commit was removed by mistake

--- skincare-quiz/src/components/pagination/Pagination.jsx
+++ skincare-quiz/src/components/pagination/Pagination.jsx
@@ -37,7 +37,7 @@ const Pagination = ({
         <span>
           <svg
             key={pageNumber}
-            className={currentPage === pageNumber ? 'active dot' : 'dot'}
+            className={currentPage === pageNumber ? 'active-dot dot' : 'dot'}
             xmlns='http://www.w3.org/2000/svg'
             width='40'
             height='40'
