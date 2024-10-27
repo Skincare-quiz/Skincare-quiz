1735155885ded414734ed050f92e05fac7c0b2a9
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-27 15:30:22 +0200

    move paginate buttons in results component

--- skincare-quiz/src/components/pagination/Pagination.jsx
+++ skincare-quiz/src/components/pagination/Pagination.jsx
@@ -1,11 +1,6 @@
 import './pagination.css';
 
-const Pagination = ({
-  productsPerPage,
-  totalProducts,
-  handlePagination,
-  currentPage,
-}) => {
+const Pagination = ({ productsPerPage, totalProducts, currentPage }) => {
   const paginationNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalProducts.length / productsPerPage); i++) {
@@ -14,22 +9,6 @@ const Pagination = ({
 
   return (
     <div className='pagination'>
-      {currentPage !== 1 && (
-        <span
-          className='paginate-btn'
-          onClick={() => handlePagination(currentPage - 1)}
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='35'
-            height='35'
-            viewBox='0 0 16 16'
-          >
-            <path d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5' />
-          </svg>
-        </span>
-      )}
-
       {paginationNumbers.map((pageNumber) => (
         <span key={pageNumber}>
           <svg
@@ -43,21 +22,6 @@ const Pagination = ({
           </svg>
         </span>
       ))}
-      {currentPage !== paginationNumbers.length && (
-        <span
-          className='paginate-btn'
-          onClick={() => handlePagination(currentPage + 1)}
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='35'
-            height='35'
-            viewBox='0 0 16 16'
-          >
-            <path d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8' />
-          </svg>
-        </span>
-      )}
     </div>
   );
 };
--- skincare-quiz/src/pages/results/ResultsPage.jsx
+++ skincare-quiz/src/pages/results/ResultsPage.jsx
@@ -61,6 +61,21 @@ function ResultsPage() {
         </div>
         <div className='products-pagination'>
           <div className='products'>
+            {currentPage !== 1 && (
+              <span
+                className='paginate-btn'
+                onClick={() => handlePagination(currentPage - 1)}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='35'
+                  height='35'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5' />
+                </svg>
+              </span>
+            )}
             {currentPosts.map((product) => (
               <div
                 className='product'
@@ -80,6 +95,21 @@ function ResultsPage() {
                 </div>
               </div>
             ))}
+            {currentPage !== 3 && (
+              <span
+                className='paginate-btn'
+                onClick={() => handlePagination(currentPage + 1)}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='35'
+                  height='35'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8' />
+                </svg>
+              </span>
+            )}
           </div>
 
           <span className='pagination'>
